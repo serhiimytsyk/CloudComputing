f71e525d27ab14c4e16a8e92cdfc4d75784c30a6
serhiimytsyk <mytsyk.s.v@gmail.com>
2024-09-23 19:48:20 -0500

    ndarray fix, less epochs

--- inference_consumer/inference_consumer.py
+++ inference_consumer/inference_consumer.py
@@ -34,7 +34,7 @@ def create_cifar10_model():
     return model
 
 model = create_cifar10_model()
-model.fit(x_train, y_train, epochs=20, batch_size=64, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=2, batch_size=64, validation_data=(x_test, y_test))
 model.save_weights('cifar10_model.weights.h5')
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print(f"Test accuracy: {test_accuracy:.4f}")
@@ -52,7 +52,7 @@ for msg in consumer:
     outputs = model.predict(data)
     prediction = np.argmax(outputs, axis=1)
     print('predicted:', prediction, '    actual:', req['GroundTruth'])
-    doc = {'ID': req['ID'], 'prediction': prediction, 'GroundTruth': req['GroundTruth'], 'Data' : req['Data']}
+    doc = {'ID': req['ID'], 'prediction': prediction}
     producer.send("prediction", value = json.dumps(doc).encode('utf-8'))
     producer.flush()
     time.sleep(1)
