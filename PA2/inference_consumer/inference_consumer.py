104c38e5b2fa749324a918e0482f960b8e2c6a9e
serhiimytsyk <mytsyk.s.v@gmail.com>
2024-10-16 15:22:37 -0500

    decreased the number of epochs to 9

--- PA2/inference_consumer/inference_consumer.py
+++ PA2/inference_consumer/inference_consumer.py
@@ -40,7 +40,7 @@ def create_cifar10_model():
 
 
 model = create_cifar10_model()
-model.fit(x_train, y_train, epochs=20, batch_size=64,
+model.fit(x_train, y_train, epochs=9, batch_size=64,
           validation_data=(x_test, y_test))
 model.save_weights('cifar10_model.weights.h5')
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
