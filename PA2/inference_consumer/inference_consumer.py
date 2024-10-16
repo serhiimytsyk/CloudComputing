39487ba5379d6d5a40233e159d9168e8dc93d024
Luka <luka.mushkudiani@vanderbilt.edu>
2024-10-16 14:42:13 -0500

    updated inference consumer docker

--- PA2/inference_consumer/inference_consumer.py
+++ PA2/inference_consumer/inference_consumer.py
@@ -7,14 +7,19 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.utils import to_categorical
 import numpy as np
 import time
+import sys
+if sys.version_info >= (3, 12, 0):
+    import six
+    sys.modules['kafka.vendor.six.moves'] = six.moves
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
-y_train = y_train.flatten() 
+y_train = y_train.flatten()
 y_test = y_test.flatten()
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
+
 
 def create_cifar10_model():
     model = Sequential([
@@ -26,15 +31,17 @@ def create_cifar10_model():
         Dense(64, activation='relu'),
         Dense(10, activation='softmax')
     ])
-    
+
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    
+
     return model
 
+
 model = create_cifar10_model()
-model.fit(x_train, y_train, epochs=20, batch_size=64, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=20, batch_size=64,
+          validation_data=(x_test, y_test))
 model.save_weights('cifar10_model.weights.h5')
 test_loss, test_accuracy = model.evaluate(x_test, y_test)
 print(f"Test accuracy: {test_accuracy:.4f}")
@@ -43,7 +50,8 @@ print(f"Test accuracy: {test_accuracy:.4f}")
 consumer = KafkaConsumer(bootstrap_servers="192.168.5.97:9092")
 consumer.subscribe(topics=["images"])
 
-producer = KafkaProducer(bootstrap_servers="192.168.5.97:9092", acks=1, api_version=(0,11,5))
+producer = KafkaProducer(
+    bootstrap_servers="192.168.5.97:9092", acks=1, api_version=(0, 11, 5))
 
 for msg in consumer:
     req = json.loads(msg.value.decode('utf-8'))
@@ -53,7 +61,7 @@ for msg in consumer:
     prediction = int(np.argmax(outputs, axis=1)[0])
     print('predicted:', prediction, '    actual:', req['GroundTruth'])
     doc = {'ID': req['ID'], 'prediction': prediction}
-    producer.send("prediction", value = json.dumps(doc).encode('utf-8'))
+    producer.send("prediction", value=json.dumps(doc).encode('utf-8'))
     producer.flush()
     time.sleep(1)
 
