36417d80811936b3f5b5f219ae332f6cdb16f3c9
Luka <luka.mushkudiani@vanderbilt.edu>
2024-11-25 14:55:11 -0600

    data from polygon

--- FinalProject/data-preparation/data-preparation.py
+++ FinalProject/data-preparation/data-preparation.py
@@ -1,2 +1,24 @@
 # use yfinance to get price data, transform it into format we want, and save in the file
 # do it for both exchange and for ML training
+import os
+import requests
+from dotenv import load_dotenv, find_dotenv
+from polygon import RESTClient
+
+load_dotenv(find_dotenv())
+POLYGON_API_KEY = os.getenv("polygon_api_key")
+
+ticker = "AAPL"
+multiplier = 1
+timespan = "second"
+_from = "2024-10-15"
+_to = "2024-11-24"
+client = RESTClient(api_key=POLYGON_API_KEY)
+
+
+# List Aggregates (Bars)
+aggs = []
+for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="second", from_="2024-11-01", to="2024-11-20", limit=50000):
+    aggs.append(a)
+
+print(aggs)
